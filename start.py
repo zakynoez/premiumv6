fad510e9dc8c4e2abce96c9e159081497b738642
zakynoez <145266413+zakynoez@users.noreply.github.com>
2023-10-14 18:51:54 +0700

    Add files via upload

---
+++ start.py
@@ -0,0 +1,61 @@
+import subprocess
+import requests,json,os,sys,random,datetime,time,re,platform
+from rich.progress import Progress,SpinnerColumn,BarColumn,TextColumn,TimeElapsedColumn
+from concurrent.futures import ThreadPoolExecutor as tred
+from time import sleep as waktu
+from rich import print as cetak
+
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m'
+N = '\x1b[0m'    
+Z = "\033[1;30m"
+sir = '\033[41m\x1b[1;97m'
+x = '\33[m' # DEFAULT
+m = '\x1b[1;91m' #RED +
+k = '\033[93m' # KUNING +
+h = '\x1b[1;92m' # HIJAU +
+hh = '\033[32m' # HIJAU -
+u = '\033[95m' # UNGU
+kk = '\033[33m' # KUNING -
+b = '\33[1;96m' # BIRU -
+p = '\x1b[0;34m' # BIRU +
+mer = '\033[1;31m'
+kun = '\033[1;33m'
+hijo = '\033[1;32m'
+biru = '\033[1;34m'
+ung = '\033[1;35m'
+puti = '\033[1;37m'
+bira = '\033[1;36m'
+xxx = '\33[m'
+bul = '••••••••••••••••••••••••••••••••••••••'
+os.system('clear')
+print(f"""
+TOOL BY GustixaCB
+
+Select Bundling :
+1. XL ( {hijo}1 B, 3 B{xxx} )
+2. POLYTRON ({hijo} 3 B{xxx} )
+3. TCL_TV ({hijo} 1 B, 1 T{xxx} )
+4. COOCCA ({hijo} 3 B, 1 T{xxx} )""")
+kdh = input("select :")
+print(f"""
+SELECT METHODE EMAIL
+1. mail10minute    2. mailpedia
+          3. secmail""")
+an = input(f"{hijo}1/2/3/d {xxx}:")
+print(f"CHANGED TO {hijo}MAILPEDIA")
+print(f"\nGUNAKAN PASSWORD ACAK/MANUAL")
+k = input(f"Y/N : ")
+print(f"\n MEMAKAI USERNAME ACAK/MANUAL")
+ok = input(f"R/M : ")
+print(f"""
+STARTING{hijo}.....{xxx}
+
+[ {mer} in process{xxx} ]
+""")
+ok = input('')
